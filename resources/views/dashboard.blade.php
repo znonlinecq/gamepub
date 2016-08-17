6739b2282830701fef48b868428510a4295378cb
zhaoning@openfin.com <znonline>
2016-08-17 11:00:07 +0800

    up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,12 @@
   <!-- Bootstrap 3.3.6 -->
   <link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="resources/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="resources/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="resources/adminlte/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="resources/adminlte/css/skins/skin-blue.min.css">
+  <link rel="stylesheet" href="resources/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="resources/dist/css/skins/skin-blue.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -167,7 +167,7 @@ desired effect
 <!-- Bootstrap 3.3.6 -->
 <script src="resources/bootstrap/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
-<script src="resources/adminlte/js/app.min.js"></script>
+<script src="resources/dist/js/app.min.js"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
